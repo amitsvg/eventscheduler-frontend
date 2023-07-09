00415a338ee7fe9e20a921b6e0d500fd19074a02
Amit Kumar <72488615+amitsvg@users.noreply.github.com>
2023-07-09 15:46:14 +0530

    done button, and its application

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,20 +1,56 @@
 import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState, useCallback } from 'react'
 import Calendar from "./components/Calendar";
 import Mytask from './components/Mytask';
 import { useNavigate } from 'react-router-dom';
+import { toast } from "react-toastify";
+
 
 const Homepage = () => {
+
     const navigate = useNavigate();
     const [allEvents, setAllEvents] = useState([]);
-    
+
+    // Toast Functions
+    const notifyA = (msg) => toast.error(msg);
+    const notifyB = (msg) => toast.success(msg);
+
+
+
+
+    // const updateTask = useCallback((id) => {
+    const updateTask = (id) => {
+        fetch('https://eventscheduler-backend.onrender.com/update', {
+        // fetch('http://localhost:8080/update', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jtoken")
+            },
+            body: JSON.stringify({
+                eventId: id
+            })
+        }).then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    notifyA(data.error);
+                } else {
+                    notifyB(`"${data.title}" :)  COMPLETED`);
+                }
+                // console.log(data)
+            })
+    }
+    // }, [])
+
+
     useEffect(() => {
         const token = localStorage.getItem("jtoken");
         if (!token) {
             navigate("/signin");
         }
         else {
-            fetch('http://localhost:8080/myevents', {
-                // fetch('https://eventscheduler-backend.onrender.com/myevents', {
+            // fetch('http://localhost:8080/myevents', {
+                fetch('https://eventscheduler-backend.onrender.com/myevents', {
                 method: "get",
                 headers: {
                     "Content-Type": "application/json",
@@ -28,13 +64,12 @@ const Homepage = () => {
                 })
                 .catch(err => console.log(err))
         }
-    }, [navigate])
-
+    }, [navigate, updateTask]);
 
     return (
         <div className='homepage' style={{ "display": "flex", "marginTop": "7%", "border": "2px solid red" }}>
             <Calendar />
-            <Mytask allEvents={allEvents} />
+            <Mytask allEvents={allEvents} handleUpdate={updateTask} />
 
         </div>
     )
--- src/components/Mytask.jsx
+++ src/components/Mytask.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+// import React, { useState } from 'react'
 
-const Mytask = ({ allEvents }) => {
+const Mytask = ({ allEvents, handleUpdate}) => {
+    // const [incomplete, setIncomplete] = useState([]);
+    // const [complete, setComplete] = useState([]);
+    
+    const complete = [];
+    const incomplete = [];
+
+    allEvents.forEach(event => {
+        if(event.completed){
+            complete.push(event);
+        }
+        else{
+            incomplete.push(event);
+        }
+    });
+
+    // console.log(allEvents);
+
     return (
         <div style={{ "padding": "0.6rem" }}>
             <h1>My Tasks are displayed here</h1>
@@ -9,10 +27,12 @@ const Mytask = ({ allEvents }) => {
                 "boxShadow": "0px 14px 60px 0px rgba(247, 112, 94, 0.4)"
             }}>
                 <ul style={{ "width": "50vw", "overflow": "auto" }}>
-                    {allEvents.map((event, index) =>
+                    {incomplete.map((event, index) =>
                         <li key={index} style={{ "border": "2px solid red", "background": "pink", "margin": "1rem", "padding": "0.6rem", "fontSize": "0.8rem" }} >
                             <p>{event.title}</p>
                             <p>{event.eventDate}</p>
+                            <p>{event._id}</p>
+                            <button onClick={() => {handleUpdate(event._id);}} >Done</button>
                         </li>
                     )}
                 </ul>
@@ -21,8 +41,8 @@ const Mytask = ({ allEvents }) => {
             <h2 style={{ "marginTop": "3rem" }}>Task done</h2>
             <div className="allTasks" style={{ "boxShadow": "0px 13px 60px 0px rgba(187, 243, 144, 0.6)" }}>
                 <ul style={{ "width": "50vw", "overflow": "auto" }}>
-                    {allEvents.map((event, index) =>
-                        <li key={index} style={{ "border": "2px solid red", "background": "pink", "margin": "1rem", "padding": "0.6rem", "fontSize": "0.8rem" }} >
+                    {complete.map((event, index) =>
+                        <li key={index} style={{ "border": "2px solid green", "background": "rgba(154, 243, 0, 0.6)", "margin": "1rem", "padding": "0.6rem", "fontSize": "0.8rem" }} >
                             <p>{event.title}</p>
                             <p>{event.eventDate}</p>
                         </li>
